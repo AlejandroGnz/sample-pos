227e2cd2fc90a21cb0c9ba2be4cf5912a1dae179
Alejandro Mora <alejandromora622@gmail.com>
2017-09-12 19:15:04 -0400

    attach all routes to resource user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,4 +4,12 @@ const userResource = require('../controllers/user/');
 
 module.exports = (app) => {
 
+  app.route('/user')
+    .get(userResource.getAll)
+    .post(userResource.create)
+  
+  app.route('/user/:id')
+    .get(userResource.getOne)
+    .put(userResource.update)
+    .delete(userResource.delete)
 }
