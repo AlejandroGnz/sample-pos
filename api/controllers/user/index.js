bea6902aee40759774270462d2a49ea4edf86b3e
Alejandro Mora <alejandromora622@gmail.com>
2017-09-12 18:57:10 -0400

    added files for user resource

---
+++ api/controllers/user/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+exports.getAll = require('./getAll')
+exports.getOne = require('./getOne')
+exports.create = require('./create')
+exports.delete = require('./delete')
+exports.update = require('./update')
---
+++ api/routes/user.route.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const userResource = require('../controllers/user/');
+
+module.exports = (app) => {
+
+}
