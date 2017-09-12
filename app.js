8ad56a450f068f435c4d36048dc057fc699c8adf
Alejandro Mora <alejandromora622@gmail.com>
2017-09-12 19:23:31 -0400

    implement body-parse dependency

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 'use strict'
 
-const express = require('express');
-const app     = express();
+const express     = require('express');
+const app         = express();
+const routes      = require('./routes/');
+const bodyParser  = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// function that register all routes
+routes(app);
 
 app.listen(3000, () => {
   console.log("running app in port " + arguments[0]);
