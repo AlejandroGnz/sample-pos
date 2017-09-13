db98a190618d55b348baa7f468da5767fcf7a627
Alejandro Mora <alejandromora622@gmail.com>
2017-09-13 00:38:45 -0400

    added get one user functionality

--- api/controllers/user/getOne.js
+++ api/controllers/user/getOne.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const mongoose = require('mongoose');
+const UserSchema = require('../../models/user.js');
+const User = mongoose.model('userSchema');
+
 module.exports = (req, res, next) => {
-  
+  User.findOne({id: req.body.id}, (err, user) => {
+    err ? res.send(err) : res.json(user);
+  })
 }
