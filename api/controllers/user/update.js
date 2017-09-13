436496ea9c0a4e76faa83e28539dcddc99ea4f63
Alejandro Mora <alejandromora622@gmail.com>
2017-09-13 01:02:33 -0400

    added update user functionality

--- api/controllers/user/update.js
+++ api/controllers/user/update.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const mongoose = require('mongoose');
+const UserSchema = require('../../models/user.js');
+const User = mongoose.model('userSchema');
+
 module.exports = (req, res, next) => {
-  
+  User.findOneAndUpdate(req.params.id, req.body, {new: true}, (err, userResult) => {
+    err ? res.send(err) : res.json(userResult);
+  })
 }
