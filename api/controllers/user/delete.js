40c525f123d21d39f5e9e3e3f2c44025a94cf2d0
Alejandro Mora <alejandromora622@gmail.com>
2017-09-13 01:06:47 -0400

    added delete user functionality

--- api/controllers/user/delete.js
+++ api/controllers/user/delete.js
@@ -1,5 +1,11 @@
 'use strict'
 
+const mongoose = require('mongoose');
+const UserSchema = require('../../models/user.js');
+const User = mongoose.model('userSchema');
+
 module.exports = (req, res, next) => {
-  
+  User.remove({_id: req.params.id}, (err, userResult) => {
+    err ? res.send(err) : res.json({message: 'User successfully deleted'});
+  })
 }
